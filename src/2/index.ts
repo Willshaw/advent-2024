794c0fb4f02f97f158509f9c6be201f0457b8099
Pete Williamson <pete.williamson@ovo.com>
2025-02-14 11:52:59 +0000

    2pt1: setup

---
+++ src/2/index.ts
@@ -0,0 +1,42 @@
+/**
+ * https://adventofcode.com/2024/day/2
+ *
+ * -- part two --
+ * find all safe lists, which are
+ * 
+ * 
+ * For example:
+ * 
+ * 7 6 4 2 1
+ * 1 2 7 8 9
+ * 9 7 6 2 1
+ * 1 3 2 4 5
+ * 8 6 4 4 1
+ * 1 3 6 7 9
+ * 
+ * the pairs and distances would be as follows:
+ * 
+ * 7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
+ * 1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
+ * 9 7 6 2 1: Unsafe because 6 2 is a decrease of 4.
+ * 1 3 2 4 5: Unsafe because 1 3 is increasing but 3 2 is decreasing.
+ * 8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
+ * 1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
+ * 
+ * 2 safe reports
+ * 
+ */
+
+import { splitFileOnNewLine } from "../utils";
+import { locationDiffTotal, similarityScore } from "./locations";
+
+const run_1 = () => {
+  const data: string[] = splitFileOnNewLine("../input/1.txt");
+  // turn ["1   2"] into [[1, 2]]
+  const locations = data.map((line) => line.replace(/\s+/g,',').split(",").map((n) => parseInt(n)));
+
+  console.log("Part 1, diff total:", locationDiffTotal(locations));
+  console.log("Part 2, similarity score:", similarityScore(locations));
+};
+
+export { run_1 };
---
+++ test/2/index.spec.js
@@ -0,0 +1,69 @@
+import { expect, test } from '@jest/globals';
+import {
+} from "../../dist/2/reports.js";
+
+const examples = [
+  {
+    total_safer_reports: 2,
+    input: [
+      [7, 6, 4, 2, 1],
+      [1, 2, 7, 8, 9],
+      [9, 7, 6, 2, 1],
+      [1, 3, 2, 4, 5],
+      [8, 6, 4, 4, 1],
+      [1, 3, 6, 7, 9],
+    ]
+  },
+];
+
+// test(`difference is calculated correctly`, () => {
+//   expect(locationIdDiff([[1],[7]],0)).toBe(6);
+//   expect(locationIdDiff([[9],[2]],0)).toBe(7);
+//   expect(locationIdDiff([[0,-3],[0,12]],1)).toBe(15);
+// });
+
+// test(`arrays can be transposed`, () => {
+//   const input = [
+//     [1,12],
+//     [3,2],
+//   ];
+//   const expected = [
+//     [1,3],
+//     [12,2],
+//   ];
+//   expect(transposeLocations(input)).toEqual(expected);
+// });
+
+// test(`arrays can be sorted`, () => {
+//   const input = [
+//     [1,12],
+//     [3,2],
+//   ];
+//   const expected = [
+//     [1,12],
+//     [2,3],
+//   ];
+//   expect(sortLocations(input)).toEqual(expected);
+// });
+
+// test(`arrays can be transposed and sorted`, () => {
+//   const input = [
+//     [1,12],
+//     [3,2],
+//   ];
+//   const expected = [
+//     [1,3],
+//     [2,12],
+//   ];
+//   expect(transposeAndSortLocations(input)).toEqual(expected);
+// });
+
+examples.forEach((example, i) => {
+  test(`correct total from input (index ${i})`, () => {
+    expect(locationDiffTotal(example.input)).toBe(example.total);
+  });
+
+  test(`similarity score from input (index ${i})`, () => {
+    expect(similarityScore(example.input)).toBe(example.similarity);
+  });
+});
